c8424e6a138524b694ae61703d1b7b2f9002168d
BruceHill <brucehillwalley@gmail.com>
2024-01-19 22:50:28 +0300

    Project Skeleton

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import AppRouter from "./router/AppRouter"
+import { grey, blueGrey } from "@mui/material/colors"
+import { Provider } from "react-redux"
+import store, { persistor } from "./app/store"
+import { ToastContainer } from "react-toastify"
+import { PersistGate } from "redux-persist/integration/react"
+
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: grey["900"],
+      },
+      secondary: {
+        main: blueGrey["900"],
+      },
+    },
+  })
   return (
-    <div>
-      <h1>React App</h1>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <AppRouter />
+          </PersistGate>
+        </Provider>
+        <ToastContainer />
+      </ThemeProvider>
+    </>
   )
 }
 
